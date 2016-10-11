377ad5da67bfbad7390b804dbe5d946e10a171eb
Shintaro Kaneko <kaneshin0120@gmail.com>
2016-10-11 10:36:00 +0000

    Fix bug for gatecli

--- cmd/gatecli/main.go
+++ cmd/gatecli/main.go
@@ -10,10 +10,13 @@ import (
 )
 
 var (
-	host = flag.String("host", "http://localhost:8080", "")
+	host    = flag.String("host", "http://localhost:8080", "")
+	channel = flag.String("c", "", "")
 )
 
 func main() {
+	flag.Parse()
+
 	// Execute: echo "foo" | go run main.go
 	body, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
@@ -21,7 +24,10 @@ func main() {
 	}
 	if _, err := http.PostForm(
 		*host,
-		url.Values{"message": {string(body)}},
+		url.Values{
+			"message":       {string(body)},
+			"slack.channel": {*channel},
+		},
 	); err != nil {
 		log.Fatal(err)
 	}
